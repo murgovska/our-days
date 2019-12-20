f244b5c0bf157b07b42b5010029dede95f77042b
murgovska <kristina.murgovska@gmail.com>
2019-12-20 14:37:43 +0100

    Commiting of build files

---
+++ precache-manifest.0a88259268781feac31b5eca9cfae8d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383c7a30b9208cebe073b06cda0b8e07",
+    "url": "/our-days/index.html"
+  },
+  {
+    "revision": "88f06c16b35553a9947a",
+    "url": "/our-days/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae27cd29d1938c066315",
+    "url": "/our-days/static/js/2.1507ac9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/our-days/static/js/2.1507ac9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88f06c16b35553a9947a",
+    "url": "/our-days/static/js/main.7a22494b.chunk.js"
+  },
+  {
+    "revision": "b253ea2e042db22f00a7",
+    "url": "/our-days/static/js/runtime-main.8fc4fb22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/our-days/static/media/logo.5d5d9eef.svg"
+  }
+]);
